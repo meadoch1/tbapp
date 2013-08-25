aa81d069bc5d328ffc2c84e7bcc1bc800093e50b
Chris Meadows <meadoch1@gmail.com>
2013-08-24 21:22:06 -0400

    Initial check in.  Runs base app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tbapp::Application.config.secret_token = '429c68e1e6bdb531f9b03e387170a7b1ce52708c55e147fda8d1e13bd5dad13d5a85414e9257299033004086ab8fbc2fe9fc1c1b1995eb51752e37391cbf288a'
